bb00bceb5047e560fc09385502d6ed22da25bf6a
chaipawn <chaiyapawn.n@gmail.com>
2023-01-02 12:17:29 +0700

    makeing widget constant with inherited widget

--- lib/counter_app/counter_app_body.dart
+++ lib/counter_app/counter_app_body.dart
@@ -1,85 +1,36 @@
+import 'package:enhanced_counter_app/counter_app/model/counter.dart';
 import 'package:enhanced_counter_app/counter_app/widgets/app_title.dart';
+import 'package:enhanced_counter_app/counter_app/widgets/counter_buttons.dart';
+import 'package:enhanced_counter_app/counter_app/widgets/counter_inherited_widget.dart';
 import 'package:enhanced_counter_app/counter_app/widgets/history_widget.dart';
 import 'package:flutter/material.dart';
 
 /// The contents of the counter app.
-class CounterAppBody extends StatefulWidget {
+class CounterAppBody extends StatelessWidget {
   /// Creates a [CounterAppBody] widget.
   const CounterAppBody({
     Key? key,
   }) : super(key: key);
 
   @override
-  State<CounterAppBody> createState() => _CounterAppBodyState();
-}
-
-class _CounterAppBodyState extends State<CounterAppBody> {
-  /// The current status of the counter.
-  int counter = 0;
-
-  /// Keeps track of the counter status when '+1' is pressed.
-  List<int> increaseHistory = [];
-
-  /// Increases the counter by 1.
-  void increase() {
-    setState(() {
-      counter++;
-      increaseHistory = List<int>.from(increaseHistory)..add(counter);
-    });
-  }
-
-  /// Decreases the counter by 1.
-  void decrease() {
-    setState(() {
-      counter--;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            // The top title
-            const AppTitle(),
+    return CounterState(
+      model: Counter(),
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              // The top title
+              AppTitle(),
 
-            // The counter
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton(
-                  key: const Key('ElevatedButton-Decrease'),
-                  onPressed: decrease,
-                  child: const Text('-'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 30,
-                    vertical: 50,
-                  ),
-                  child: Text(
-                    '$counter',
-                    style: const TextStyle(
-                      fontSize: 20,
-                      color: Colors.blue,
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  key: const Key('ElevatedButton-Increase'),
-                  onPressed: increase,
-                  child: const Text('+'),
-                ),
-              ],
-            ),
+              // The counter buttons
+              CounterButtons(),
 
-            // The history
-            HistoryWidget(
-              increasesHistory: increaseHistory,
-            ),
-          ],
+              // The history
+              HistoryWidget(),
+            ],
+          ),
         ),
       ),
     );
--- lib/counter_app/widgets/history_widget.dart
+++ lib/counter_app/widgets/history_widget.dart
@@ -1,14 +1,11 @@
+import 'package:enhanced_counter_app/counter_app/widgets/counter_inherited_widget.dart';
 import 'package:flutter/material.dart';
 
 /// This widget keeps track of the counter values whenever it is increased.
 class HistoryWidget extends StatefulWidget {
-  /// The counters history.
-  final List<int> increasesHistory;
-
-  /// Creates an [HistoryWidget] from the given [increasesHistory].
+  /// Creates an [HistoryWidget] widget.
   const HistoryWidget({
     Key? key,
-    required this.increasesHistory,
   }) : super(key: key);
 
   @override
@@ -17,44 +14,10 @@ class HistoryWidget extends StatefulWidget {
 
 /// The state of the [HistoryWidget] widget.
 class _HistoryWidgetState extends State<HistoryWidget> {
-  /// Manually caching the list.
-  late ListView list = buildList();
-
-  /// Building the list.
-  ListView buildList() {
-    return ListView.separated(
-      key: const Key('HistoryWidget-ListView'),
-      scrollDirection: Axis.horizontal,
-      itemCount: widget.increasesHistory.length,
-      itemBuilder: (_, index) {
-        return Card(
-          elevation: 4,
-          shadowColor: Colors.blueAccent,
-          child: SizedBox(
-            width: 40,
-            height: 40,
-            child: Center(
-              child: Text('${widget.increasesHistory[index]}'),
-            ),
-          ),
-        );
-      },
-      separatorBuilder: (_, __) => const SizedBox(width: 10),
-    );
-  }
-
-  @override
-  void didUpdateWidget(covariant HistoryWidget oldWidget) {
-    super.didUpdateWidget(oldWidget);
-
-    if (widget.increasesHistory.length != oldWidget.increasesHistory.length) {
-      // Updating the list
-      list = buildList();
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
+    final counterState = CounterState.of(context).model;
+
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
@@ -70,7 +33,30 @@ class _HistoryWidgetState extends State<HistoryWidget> {
             ),
             child: SizedBox(
               height: 40,
-              child: list,
+              child: AnimatedBuilder(
+                animation: counterState,
+                builder: (context, _) {
+                  return ListView.separated(
+                    key: const Key('HistoryWidget-ListView'),
+                    scrollDirection: Axis.horizontal,
+                    itemCount: counterState.history.length,
+                    itemBuilder: (_, index) {
+                      return Card(
+                        elevation: 4,
+                        shadowColor: Colors.blueAccent,
+                        child: SizedBox(
+                          width: 40,
+                          height: 40,
+                          child: Center(
+                            child: Text('${counterState.history[index]}'),
+                          ),
+                        ),
+                      );
+                    },
+                    separatorBuilder: (_, __) => const SizedBox(width: 10),
+                  );
+                },
+              ),
             ),
           ),
         ),
